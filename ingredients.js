80d4a51596ab806899094ca0034afa081aa1e609
Path-may1 <80159654+Path-may1@users.noreply.github.com>
2021-04-02 01:43:09 +0000

    ingredients loop challenge

---
+++ ingredients.js
@@ -0,0 +1,17 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var i = 0;
+
+while(i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+for(var j =0; j < ingredients.length; j++){
+  console.log(ingredients[j]);
+}
+  
+for (let index = ingredients.length - 1; index >= 0; index--) {
+    const element = ingredients[index];
+    console.log(element);
+}
+  
